e50973a674f8ea52ff191a28423f58edc84106e5
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2024-01-24 23:13:29 +0530

    Setup express js and typescript🚀

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on Port:${port}`);
+});
